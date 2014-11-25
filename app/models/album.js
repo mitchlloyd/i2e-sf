f02015a247fd88619cfeeb7e625cb120aed96bc7
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-11-25 14:28:07 -0500

    Finish step 8

--- app/models/album.js
+++ app/models/album.js
@@ -1,10 +1,16 @@
 import DS from 'ember-data';
+import Ember from 'ember';
 const { Model, attr, hasMany } = DS;
+const { computed } = Ember;
 
 export default Model.extend({
   artwork: attr('string'),
   name: attr('string'),
   artist: attr('string'),
   isExplicit: attr('boolean'),
-  songs: hasMany('song')
+  songs: hasMany('song'),
+
+  songDurations: computed.mapBy('songs', 'duration'),
+  totalDuration: computed.sum('songDurations'),
+  songCount: computed.reads('songs.length')
 });
