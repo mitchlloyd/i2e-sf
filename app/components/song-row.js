3bbf5d7a695afb186c5b45b8ff02753a9a02747a
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-12-16 20:53:42 -0500

    Finish step 10

--- app/components/song-row.js
+++ app/components/song-row.js
@@ -1,14 +1,19 @@
 import Ember from 'ember';
+const { inject } = Ember;
 
 export default Ember.Component.extend({
   isPlaying: false,
 
+  player: inject.service(),
+
   actions: {
     play() {
+      this.get('player').play(this.get('attrs.song'));
       this.set('isPlaying', true);
     },
 
     pause() {
+      this.get('player').pause();
       this.set('isPlaying', false);
     }
   }
---
+++ app/services/player.js
@@ -0,0 +1,36 @@
+import Ember from 'ember';
+const { run, on } = Ember;
+
+export default Ember.Service.extend({
+  isPlaying: false,
+  audioElement: null,
+
+  setupAudioElement: on('init', function() {
+    let el = document.createElement('audio');
+    el.addEventListener('play', run.bind(this, 'didStartPlaying'));
+    el.addEventListener('pause', run.bind(this, 'didPause'));
+    this.set('audioElement', el);
+  }),
+
+  play(song) {
+    this.set('audioElement.src', song.get('url'));
+    this.get('audioElement').play();
+  },
+
+  pause() {
+    this.get('audioElement').pause();
+  },
+
+  didStartPlaying() {
+    this.set('isPlaying', true);
+  },
+
+  didPause() {
+    this.set('isPlaying', false);
+  },
+
+  willDestroy() {
+    this.get('audioElement').pause();
+    this.set('audioElement.src', '');
+  }
+});
