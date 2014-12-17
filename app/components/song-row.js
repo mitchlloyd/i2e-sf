3b07ffb4328fc1b4988c9c5824c00ce62994bfc3
Mitch Lloyd <mitch.lloyd@gmail.com>
2014-12-16 20:53:42 -0500

    Finish step 10

--- app/components/song-row.js
+++ app/components/song-row.js
@@ -1,15 +1,20 @@
 import Ember from 'ember';
+var inject = Ember.inject;
 
 export default Ember.Component.extend({
   tagName: 'tr',
   isPlaying: false,
 
+  player: inject.service(),
+
   actions: {
     play() {
+      this.get('player').play(this.get('song'));
       this.set('isPlaying', true);
     },
 
     pause() {
+      this.get('player').pause();
       this.set('isPlaying', false);
     }
   }
